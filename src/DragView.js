31c6c5eb85f0d6c550f77a45d9e6d14ed240e3dc
ivy <unjust@gmail.com>
2020-09-17 23:31:14 -0500

    animation with no-drag-over reset

--- src/DragView.js
+++ src/DragView.js
@@ -3,45 +3,40 @@ import DragItem from './DragItem';
 import PropTypes from 'prop-types';
 
 const DragView = ({ items, onDropCallback }) => {
-  const [ dragInProcess, setDragInProcess ] = useState(false);
   const [ draggedIndex, setDraggedIndex ] = useState();
 
   const onDragOver = (e) => {
     e.preventDefault(); // what is this default
     e.dataTransfer.dropEffect = "move";
-    setDragInProcess(true);
-    // setDraggedOverIndex(e.currentTarget)
   };
 
   const onDragStart = (i) => {
     setDraggedIndex(i);
+    console.log(i);
   }
 
   const onDragEnd = () => {
-    setDraggedIndex(null);
+    setDraggedIndex();
+    console.log("drag end", draggedIndex);
   }
 
   const onDrop = (e) => {
     e.preventDefault();
-    setDragInProcess(false);
-
     const droppedIndex = e.target.dataset.index;
     const draggedItem = items[draggedIndex];
-
     items.splice(draggedIndex, 1);
-
     if (droppedIndex) {
       items.splice(parseInt(droppedIndex), 0, draggedItem);
     } else {
       items.push(draggedItem);
     }
-
     onDropCallback(items);
   };
 
   return (
     <section
       id="drag-view"
+      className={ draggedIndex >= 0 ? '' : 'no-drag-state' }
       onDragLeave={() => {}}
       onDragOver={onDragOver}
       onDrop={onDrop}>
@@ -49,7 +44,7 @@ const DragView = ({ items, onDropCallback }) => {
       {items.map((item, i) => 
         <DragItem key={`q${i}`}
           index={i}
-          text={item} 
+          text={item}
           onDragStartCallback={onDragStart}
           onDragEndCallback={onDragEnd} />
       )}
