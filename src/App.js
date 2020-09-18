ca6215f5c008f32faac475ced516aed69a1bc8c2
ivy <unjust@gmail.com>
2020-09-17 22:02:38 -0500

    json view updating, setting model data

--- src/App.js
+++ src/App.js
@@ -4,21 +4,37 @@ import JSONView from './JSONView';
 
 const initialItems = [ "Hello 1", "Something something 2", "Que tal 3", "WTF 4???", "the end 5" ];
 
+const isValidJSON = json => {
+  try {
+    JSON.parse(json);
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
 function App() {
   const [ validItemsData, setValidItemsData ] = useState(initialItems);
-
+  const [ jsonViewText, setJSONViewText ] = useState(JSON.stringify(initialItems));
+  
   const onDragViewChange = (items) => {
     setValidItemsData(items);
   }
 
   const onJSONViewChange = (text) => {
-    setValidItemsData(JSON.parse(text));
+    
+    setJSONViewText(text);
+
+    const isValid = isValidJSON(text);
+    if (isValid) {
+      setValidItemsData(JSON.parse(text));
+    }
   }
 
   return (
     <div id="container">
       <DragView items={validItemsData} onDropCallback={onDragViewChange} />
-      <JSONView text={JSON.stringify(validItemsData)} onChangeCallback={onJSONViewChange} />
+      <JSONView text={jsonViewText} onChangeCallback={onJSONViewChange} />
     </div>
   );
 }
