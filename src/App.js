4829d7e1c8bfb8950ff5e61e40553e60822d7625
ivy <unjust@gmail.com>
2020-09-17 22:04:40 -0500

    drag order reflected in json, prettify

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import JSONView from './JSONView';
 
 const initialItems = [ "Hello 1", "Something something 2", "Que tal 3", "WTF 4???", "the end 5" ];
 
-const isValidJSON = json => {
+const isValidJSON = (json) => {
   try {
     JSON.parse(json);
     return true;
@@ -12,19 +12,19 @@ const isValidJSON = json => {
     return false;
   }
 }
+const prettifyJSON = (json) => JSON.stringify(json, null, "\t");
 
 function App() {
   const [ validItemsData, setValidItemsData ] = useState(initialItems);
-  const [ jsonViewText, setJSONViewText ] = useState(JSON.stringify(initialItems));
+  const [ jsonViewText, setJSONViewText ] = useState(prettifyJSON(initialItems));
   
   const onDragViewChange = (items) => {
     setValidItemsData(items);
+    setJSONViewText(prettifyJSON(items));
   }
 
   const onJSONViewChange = (text) => {
-    
     setJSONViewText(text);
-
     const isValid = isValidJSON(text);
     if (isValid) {
       setValidItemsData(JSON.parse(text));
